9da2140be28e63deea2d6e2ca85d1c27d3de7df5
unknown <loick701@gmail.com>
2021-12-06 16:16:23 +0100

    changes for V2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+importReactfrom'react';
+importlogofrom'./logo.svg';
+import'./App.css';
 
-function App() {
-  return (
+functionApp(){
+  return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img src={logo}className="App-logo"alt="logo"/>
+        <h1>HellofromV2</h1>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </header>
+        </div>
+        );
 }
 
 export default App;
